b42b2c395e438674cbf8b24a1aac1cbed8e1aab4
Nicholas Clark <clarkno@mail.uc.edu>
2016-09-10 14:20:53 -0400

    Boxplot p-value branches almost done.

--- functions/drawBox.R
+++ functions/drawBox.R
@@ -16,16 +16,15 @@ drawBox <- function(input, values) {
   
   boxplot_data = full_data[full_data[[ input$pick_box_x ]] %in% input$pick_box_factors,]
   boxplot_data[[ input$pick_box_x ]] = factor(boxplot_data[[ input$pick_box_x ]])
-  if(!is.null(input$factorB)) {
+  if(!is.null(input$factorB) & !is.null(input$factorA)) {
     for(i in 1:length(input$factorB)) {
       boxplot_data[[ input$pick_box_x ]] = relevel(boxplot_data[[ input$pick_box_x ]], input$factorB[i])
     }
-  }
-  if(!is.null(input$factorA)) {
     for(i in 1:length(input$factorA)) {
       boxplot_data[[ input$pick_box_x ]] = relevel(boxplot_data[[ input$pick_box_x ]], input$factorA[i])
     }
   }
+
   x_factor = factor(get(input$pick_box_x, envir = as.environment(boxplot_data)))
   y_variable = get(parameter_choice, envir = as.environment(boxplot_data))
   point_color = factor(get(input$pick_box_point_color, envir = as.environment(boxplot_data)))
@@ -42,47 +41,60 @@ drawBox <- function(input, values) {
     #test_gg<<- q
     p1 = plotly_build(p)
     test_box <<- p1
-    # Get y range:
-    top_y = p1[[2]]$yaxis$range[2]
-    print('topy1')
-    print(top_y)
-    bottom_y = p1[[2]]$yaxis$range[1]
-    total_y_range = top_y - bottom_y
-    # Get top of boxplot whiskers
-    whiskers = NULL
-    print('length')
-    print(length(levels(x_factor)))
-    for(i in 1:length(levels(x_factor))) {
-      whiskers[i] = fivenum(p1[[1]][[i]]$y)[5]
-    }
-    top_whisker = max(whiskers, na.rm = TRUE)
-    y_range = top_y - top_whisker
-    print('y_range1')
-    print(y_range)
-    print('total_y_range')
-    print(total_y_range)
-    zoom_out = FALSE
-    if(y_range < .5*total_y_range) {
-      zoom_out = TRUE
-      top_y = top_y + .2*total_y_range
-      y_range = top_y - top_whisker
-    }
-    print('topy2')
-    print(top_y)
-    lh = top_whisker + total_y_range*(.1)
-    ll = lh - total_y_range*(.05)
-    print(values$wilcox)
-    print(lh)
-    print(ll)
-    df1 <- data.frame(a = c(1,1,2,2), b = c(ll,lh,lh,ll))
+   
     if(!is.null(values$wilcox)) {
-      p = p + annotate("text", x = 1.5, y = lh + total_y_range*(.025), label = paste("p = ",values$wilcox)) + geom_segment(x = 1, y = lh, xend = 2, yend = lh) + geom_segment(x = 1, y = ll, xend = 1, yend = lh) + geom_segment(x = 2, y = ll, xend = 2, yend = lh)
-        #geom_path(x = c(rep(1, 56),rep(2, 56)), y = c(rep(ll, 28),rep(lh,56),rep(ll, 28)))
-      q = q + annotate("text", x = 1.5, y = lh, label = values$wilcox) + geom_line(data = df1, aes(x = a, y = b))
+      # Get y range:
+      top_y = p1[[2]]$yaxis$range[2]
+      print('topy1')
+      print(top_y)
+      bottom_y = p1[[2]]$yaxis$range[1]
+      total_y_range = top_y - bottom_y
+      # Get top of boxplot whiskers
+      whiskers = NULL
+      #for(i in 1:length(levels(x_factor))) {
+      len = length(input$factorA) + length(input$factorB)
+      for(i in 1:len) {
+        whiskers[i] = fivenum(p1[[1]][[i]]$y)[5]
+      }
+      top_whisker = max(whiskers, na.rm = TRUE)
+      y_range = (top_y - top_whisker)/total_y_range
+      if(y_range < .25) {
+        top_y = top_whisker + .25*total_y_range
+        #y_range = top_y - top_whisker
+      }
+      lh = top_whisker + total_y_range*(.1)
+      bump = total_y_range*(.05)
+      ll = lh - bump
+      lenA = length(input$factorA)
+      lenB = length(input$factorB)
+      if(lenA == 1 & lenB == 1) {
+        p = p + annotate("text", x = 1.5, y = lh + bump/2, label = paste("p = ",values$wilcox)) + geom_segment(x = 1, y = lh, xend = 2, yend = lh) + geom_segment(x = 1, y = ll, xend = 1, yend = lh) + geom_segment(x = 2, y = ll, xend = 2, yend = lh)
+        
+        df1 <- data.frame(a = c(1,1,2,2), b = c(ll,lh,lh,ll))
+        q = q + annotate("text", x = 1.5, y = lh, label = values$wilcox) + geom_line(data = df1, aes(x = a, y = b))
+      } else if(lenA > 1 & lenB == 1) {
+        # p = p + annotate("text", x = ((lenA + 1) + ((lenA+1)/2))/2, y = lh + 2*bump, label = paste("p = ",values$wilcox)) + 
+        #   geom_segment(x = 1, y = lh, xend = lenA, yend = lh) + geom_segment(x = 1, y = ll, xend = 1, yend = lh) + geom_segment(x = lenA, y = ll, xend = lenA, yend = lh) +
+        #   geom_segment(x = (lenA+1)/2, y = lh + bump, xend = lenA + 1, yend = lh + bump) + geom_segment(x = (lenA+1)/2, y = lh, xend = (lenA+1)/2, yend = lh + bump) + geom_segment(x = lenA+1, y = ll, xend = lenA+1, yend = lh + bump)
+        
+        # df1 <- data.frame(a = c(1,1,2,2), b = c(ll,lh,lh,ll))
+        # q = q + annotate("text", x = 1.5, y = lh, label = values$wilcox) + geom_line(data = df1, aes(x = a, y = b))
+      } else if(lenA == 1 & lenB > 1) {
+        p = p + annotate("text", x = 1.25 + .25*lenB, y = lh + 2*bump, label = paste("p = ",values$wilcox)) + 
+          geom_segment(x = 1, y = lh+bump, xend = .5*lenB + 1.5, yend = lh+bump) + geom_segment(x = 1, y = ll, xend = 1, yend = lh+bump) + geom_segment(x = 1.5+.5*lenB, y = lh, xend = 1.5+.5*lenB, yend = lh+bump) +
+          geom_segment(x = 2, y = lh, xend = lenB + 1, yend = lh) + geom_segment(x = 2, y = ll, xend = 2, yend = lh) + geom_segment(x = lenB+1, y = ll, xend = lenB+1, yend = lh)
+        
+        # df1 <- data.frame(a = c(1,1,2,2), b = c(ll,lh,lh,ll))
+        # q = q + annotate("text", x = 1.5, y = lh, label = values$wilcox) + geom_line(data = df1, aes(x = a, y = b))
+      }
+      
+      p = plotly_build(p)
+      p[[2]]$yaxis$range[2] = top_y
+    } else {
+      p = plotly_build(p)
     }
     plotScatter_box <<- q
-    p = plotly_build(p)
-    p[[2]]$yaxis$range[2] = top_y
+    
     # Current CRAN version of plotly (3.6.0) uses p$data
     # Latest github version of plotly (4.3.5) uses p$x$data
     if(is.null(p$data)) {
